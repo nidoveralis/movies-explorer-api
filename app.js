19e5c4fde1c2bbfc32f1bc2412d8cf68eeb56468
Nidoveralis <nidoveralis@gmail.com>
2022-12-01 23:36:07 +0400

    feat: cors

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 const express = require('express');
-const {error} = require('celebrate');
+const {errors} = require('celebrate');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const {PORT=3000} = process.env;
-const app = express();
 const router = require('./routes/router');
 const {errorLogger, requestLogger} = require('./middlewares/logger');
 const errorsHandler = require('./middlewares/errorsHandler');
 
-cors
+const {PORT=3000} = process.env;
+
+const allowedCors = [
+  'https://movie.diakova.nomoredomains.club',
+  'http://movie.diakova.nomoredomains.club',
+  'https://api.movie.diak.nomoredomains.club',
+  'http://api.movie.diak.nomoredomains.club',
+  'http://localhost:3000',
+];
+
+const app = express();
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const allowMethods = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', 'true');
+  }
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    res.header('Access-Control-Allow-Methods', allowMethods);
+    return res.end();
+  }
+  return next();
+});
+
+//cors
+//api.movie.diak.nomoredomains.club
+//movie.diakova.nomoredomains.club
+//51.250.87.93
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
