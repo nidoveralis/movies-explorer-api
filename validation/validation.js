6c72a97b78f37e02d4c83074ab092a31b2cf86bc
Nidoveralis <nidoveralis@gmail.com>
2022-12-02 00:25:49 +0400

    fix: joi bug

--- validation/validation.js
+++ validation/validation.js
@@ -4,7 +4,7 @@ const linkValidation = require('../constants');
 module.exports.validationSingIn = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi().string().required(),
+    password: Joi.string().required(),
   }),
 });
 
@@ -12,30 +12,30 @@ module.exports.validationSingUp = celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
     email: Joi.string().required().email(),
-    password: Joi().string().required(),
+    password: Joi.string().required(),
   }),
 });
 
 module.exports.validationEditUser = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi().string().required(),
+    password: Joi.string().required(),
   }),
 });
 
 module.exports.validationAddedMovie = celebrate({
   body: Joi.object().keys.apply({
-    country: Joi().string().require(),
-    director: Joi().string().require(),
-    duration: Joi().string().require(),
-    year: Joi().number().require(),
-    description: Joi().string().require(),
-    image: Joi().string().require().pattern(linkValidation),
-    trailer: Joi().string().require().pattern(linkValidation),
-    nameRU: Joi().string().require(),
-    nameEN: Joi().string().require(),
-    thumbnail: Joi().string().require(),
-    id: Joi().number().require(),
+    country: Joi.string().require(),
+    director: Joi.string().require(),
+    duration: Joi.string().require(),
+    year: Joi.number().require(),
+    description: Joi.string().require(),
+    image: Joi.string().require().pattern(linkValidation),
+    trailer: Joi.string().require().pattern(linkValidation),
+    nameRU: Joi.string().require(),
+    nameEN: Joi.string().require(),
+    thumbnail: Joi.string().require(),
+    id: Joi.number().require(),
   }),
 });
 
