022608d0587f13fd43f3a994a365562237f431a9
Nidoveralis <nidoveralis@gmail.com>
2022-12-02 11:00:18 +0400

    fix: router user

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { getUser, editUser } = require('../controllers/user');
+const { getUserMe, editUser } = require('../controllers/user');
 const { validationEditUser } = require('../validation/validation');
 
-router.get('/', getUser);
+router.get('/me', getUserMe);
 router.patch('/me', validationEditUser, editUser);
 
 module.exports = router;
