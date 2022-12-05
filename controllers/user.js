01147ef5c1ffb8fe16fd7d9b7a4bcac45b44e77a
Nidoveralis <nidoveralis@gmail.com>
2022-12-05 13:39:48 +0400

    fix: jwt

--- controllers/user.js
+++ controllers/user.js
@@ -30,7 +30,6 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCreditals({ email, password })
     .then((user) => {
-      console.log(NODE_ENV);
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.cookie('jwt', token, { maxAge: 3600000, httpOnly: true });
       res.status(200).send({ token });
--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,6 @@ const IncorrectMailOrPassword = require('../errors/IncorrectMailPassword');
 
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
-  console.log(JWT_SECRET, NODE_ENV);
   if (!token) {
     next(new IncorrectMailOrPassword('Необходима авторизация.'));
     return;
@@ -12,7 +11,6 @@ module.exports = (req, res, next) => {
   let payload;
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
-    // JWT_SECRET=517ba74d3bc900db5f820165235d63fb84d69f34bba38b9685be361783d8996f
   } catch (err) {
     next(new IncorrectMailOrPassword('Необходима авторизация.'));
     return;
